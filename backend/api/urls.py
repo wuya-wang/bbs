cd76b532183396c6ef381cad6bf7e7430b92300d
colcat <colcat_chenxi@hotmail.com>
2021-06-29 14:42:48 +0800

    后端：安装第三方库

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -14,9 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    re_path(r'^api/', include("api.urls"))
+
 ]
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # App
+    'api',
+
+    # 第三方库
+    'rest_framework'
+
 ]
 
 MIDDLEWARE = [
@@ -103,15 +110,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
